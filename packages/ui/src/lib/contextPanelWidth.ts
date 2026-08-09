a4e3d8da461e067e1e74cff297ed0191fda3e211
Val <linkguardian878@gmail.com>
2026-08-09 22:08:37 +0800

    style(ui): 2-space indent in contextPanelWidth helpers

--- packages/ui/src/lib/contextPanelWidth.ts
+++ packages/ui/src/lib/contextPanelWidth.ts
@@ -1,15 +1,15 @@
 export type ContextPanelWidthInput = {
-    embeddedWidth: number | undefined;
-    manualWidth: number | undefined;
-    widthFraction: number;
-    fallbackBase: number;
-    clamp: (width: number) => number;
+  embeddedWidth: number | undefined;
+  manualWidth: number | undefined;
+  widthFraction: number;
+  fallbackBase: number;
+  clamp: (width: number) => number;
 };
 
 export const resolveContextPanelWidth = (input: ContextPanelWidthInput): number => {
-    const { embeddedWidth, manualWidth, widthFraction, fallbackBase, clamp } = input;
-    if (embeddedWidth !== undefined) {
-        return embeddedWidth;
-    }
-    return clamp(manualWidth ?? Math.round(widthFraction * fallbackBase));
+  const { embeddedWidth, manualWidth, widthFraction, fallbackBase, clamp } = input;
+  if (embeddedWidth !== undefined) {
+    return embeddedWidth;
+  }
+  return clamp(manualWidth ?? Math.round(widthFraction * fallbackBase));
 };
